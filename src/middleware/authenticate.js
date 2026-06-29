1b6092e84ecf4237de18e2942ac0b353f63400df
Barry Boy <odorobarry@gmail.com>
2026-06-29 15:09:44 +0300

    fixed typeError leading to reverse login

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -2,27 +2,31 @@ import { verifyToken } from "../utils/jwt.js";
 import { AppError } from "../errors/AppError.js";
 
 function authenticate(req, res, next) {
-  try{
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-      throw  new AppError("No token provided", 401);
-    }
-    //Expect bearer token
-    const [scheme,token] = authHeader.split(" ")[1];
-    if (!scheme == "Bearer"||!token) {
-      throw new AppError("Invalid authorization header", 401);
-    }
-    const decoded = verifyToken(token);
-    if (!decoded.userId || !decoded.role || !decoded.businessId) {
-          throw new AppError("Invalid token payload", 401);
-    }
+    try {
+        const authHeader = req.headers.authorization;
 
-    req.user = decoded;
-    next();
-  }catch(error){
-    next( new AppError("Invalid or expired token", 401));
-  }
+        if (!authHeader) {
+            throw new AppError("Authentication required", 401);
+        }
 
+        const [scheme, token] = authHeader.split(" ");
+
+        if (scheme !== "Bearer" || !token) {
+            throw new AppError("Invalid authorization header", 401);
+        }
+
+        const decoded = verifyToken(token);
+
+        if (!decoded.userId || !decoded.businessId || !decoded.role) {
+            throw new AppError("Invalid token", 401);
+        }
+
+        req.user = decoded;
+
+        next();
+    } catch {
+        next(new AppError("Invalid or expired token", 401));
+    }
 }
 
 export { authenticate };
